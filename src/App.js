c0079318254e0dd7af723d459f283a68103c737b
Danny Espinoza <d.espinosa@extraspace.com>
2024-09-08 20:25:12 -0600

    Add login screen

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from 'react';
-import Blog from './components/Blog';
-import Header from './components/Header';
-import Sidebar from './components/Sidebar';
 import supabase from './components/supabaseClient';
 import Login from './components/Login';
  
@@ -23,21 +19,7 @@ function App() {
 
   return (
     <div className="App">
-        {isLoggedIn ? (
-        <>
-          <Header />
-          <div className="row">
-            <div className="col s3">
-              <Sidebar />
-            </div>
-            <div className="col s9">
-              <Blog />
-            </div>
-          </div>
-        </>
-      ) : (
-        <Login onLogin={handleLogin} />
-      )}
+       <Login onLogin={handleLogin} />
     </div>
   );
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -3,50 +3,112 @@ import supabase from './supabaseClient';
 
 const Login = ({ onLogin }) => {
   const [email, setEmail] = useState('');
+  const [loading, setLoading] = useState(false);
   const [password, setPassword] = useState('');
   const [error, setError] = useState(null);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const { error } = await supabase.auth.signInWithPassword({
+    setLoading(true);
+    setError(null);
+
+    const { data, error } = await supabase.auth.signInWithPassword({
       email,
       password,
     });
 
     if (error) {
-      setError('Failed to login. Please check your credentials.');
+      setError(error.message);
     } else {
-      onLogin();
+      console.log('Logged in:', data);
     }
+    setLoading(false);
   };
 
   return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Email:</label>
-          <input 
-            type="email" 
-            value={email} 
-            onChange={(e) => setEmail(e.target.value)} 
-            required 
-          />
+    <div style={{ display: 'flex', minHeight: '100vh', flexDirection: 'column' }}>
+    <main style={{ flex: '1 0 auto' }}>
+      <div className="section"></div>
+      <center>
+        <img
+          className="responsive-img"
+          style={{ width: '250px' }}
+          src="https://i.imgur.com/ax0NCsK.gif"
+          alt="login illustration"
+        />
+        <div className="section"></div>
+        <h5 className="indigo-text">Please, login into your account</h5>
+        <div className="section"></div>
+
+        <div className="container">
+          <div
+            className="z-depth-1 grey lighten-4 row"
+            style={{
+              display: 'inline-block',
+              padding: '32px 48px 0px 48px',
+              border: '1px solid #EEE',
+            }}
+          >
+            <form className="col s12" onSubmit={handleSubmit}>
+              <div className="row">
+                <div className="input-field col s12">
+                  <input
+                    className="validate"
+                    type="email"
+                    name="email"
+                    id="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                  />
+                  <label htmlFor="email">Enter your email</label>
+                </div>
+              </div>
+
+              <div className="row">
+                <div className="input-field col s12">
+                  <input
+                    className="validate"
+                    type="password"
+                    name="password"
+                    id="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                  />
+                  <label htmlFor="password">Enter your password</label>
+                </div>
+                <label style={{ float: 'right' }}>
+                  <a className="pink-text" href="#!">
+                    <b>Forgot Password?</b>
+                  </a>
+                </label>
+              </div>
+
+              <br />
+              <center>
+                <div className="row">
+                  <button
+                    type="submit"
+                    name="btn_login"
+                    className="col s12 btn btn-large waves-effect pink"
+                  >
+                    Login
+                  </button>
+                </div>
+              </center>
+            </form>
+          </div>
         </div>
-        <div>
-          <label>Password:</label>
-          <input 
-            type="password" 
-            value={password} 
-            onChange={(e) => setPassword(e.target.value)} 
-            required 
-          />
-        </div>
-        {error && <p>{error}</p>}
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
+        <a href="#!">Create account</a>
+      </center>
+
+      <div className="section"></div>
+      <div className="section"></div>
+    </main>
+  </div>
+);
 };
+
 
 export default Login;
